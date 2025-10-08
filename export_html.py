184c89b5dbb6c6d3ee776e3ea69317d5ce70f87a
Victor <manuelsarutobi@hotmail.com>
2025-10-08 13:20:24 -0600

    Actualización automática (2025-10-08 13:20)

--- export_html.py
+++ export_html.py
@@ -79,6 +79,7 @@ if (!clave || clave.trim() !== "{PASSWORD}") {{
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.2, maximum-scale=1.5, user-scalable=no">
     <title>Ficha Técnica - {serial}</title>
     <link rel="stylesheet" href="style.css">
     <link rel="icon" href="qualtec.ico" type="image/x-icon">
